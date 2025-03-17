4a1ff53954b971b6ddfd27250d4a47c211f4d70e
ktunga <karthikeya.tunga@factset.com>
2025-03-18 01:18:52 +0530

    (@feat) - Addition of extract part for the data set

--- src/core/extract.py
+++ src/core/extract.py
@@ -1,1 +1,41 @@
-import pyspark
+import polars as pl
+    
+def fetch_data(path_dataset, path_tasks = None, file_type = None):
+    if file_type in ['xls', 'xlsx']:
+        df_data, df_tasks = fetch_excel(path_dataset, path_tasks)
+    elif file_type == 'csv':
+        df_data, df_tasks = fetch_csv(path_dataset, path_tasks)
+    else:
+        raise TypeError
+    return df_data, df_tasks
+    
+def fetch_csv(path_dataset, path_tasks):
+    df_data = pl.read_csv(path_dataset, has_header=True)
+    df_tasks = pl.read_csv(path_tasks, has_header=True)
+    return df_data, df_tasks
+    
+
+def fetch_excel(path_dataset, path_tasks):
+    df_data = pl.read_excel(path_dataset, has_header=True)
+    df_tasks = pl.read_excel(path_tasks, has_header=True)
+    return df_data, df_tasks 
+
+
+#tasks = r"C:\Users\ktunga\OneDrive - FactSet\Documents\weighting_scheme.xlsx"
+
+"""
+CSV
+Excel
+Parquet
+ORC
+JSON
+Avro
+Text File
+Log Files
+XML
+YAML
+HTML
+SQL Dump
+HDF5
+Feather
+Protocol Buffers"""
--- src/main.py
+++ src/main.py
@@ -2,21 +2,20 @@ import argparse
 from core import run_pipeline
 import configparser
 
-
 # Load environment.ini
 config = configparser.ConfigParser()
 config.read("config/environment.ini")
 
 def main(args):
     path_dataset = args.dataset if args.dataset else config.get("PATH","DATASET_PATH")
-    path_tasks = args.dataset if args.dataset else config.get("PATH","TASKS_PATH")
-    
-    run_pipeline(path_dataset = path_dataset, path_tasks = path_tasks ) 
+    path_tasks = args.dataset if args.dataset else config.get("PATH","TASKS_PATH")   
+    if args.task_mode is "interactive":
+        run_pipeline(path_dataset = path_dataset)
         
 if __name__ == "__main__":
     # Parse user arguments (dataset path + tasks file)
     parser = argparse.ArgumentParser(description="Automated Data Processing Tool")
     parser.add_argument("--dataset", type=str, required=False, help="Path to input dataset")
-    parser.add_argument("--tasks", type=str, required=False, help="Path to JSON file with tasks")
+    parser.add_argument("--task_mode", type=str, required=False, help="Path to JSON file with tasks")
     args = parser.parse_args()
     main(args)
